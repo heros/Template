0bce7f8ba7cfe63b83061130d2fef0ea059c0a0c
Flakee <elliotb12345@gmail.com>
2014-04-11 22:59:34 -0700

    Added the files
    Released

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	include("settings.php");
+?>
+ <head>
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon"/>
+	<link rel="stylesheet" href="style.css">
+	<title>SA-MP Template</title>
+ </head>
+ 
+ <body>
+	<a href="index.php"><div id="tab1" >Home</div></a>
+	<a href="forum.php"><div id="tab2" >Forum</div></a>
+	<a href="online.php"><div id="tab3" >UCP</div></a>
+	<div id="headdiv" ></div>
+	
+	<div id="bodydiv" >
+	<h2>Welcome to <?php echo SVRNAME; ?></h2><p>Add some general server information blah blah</p>
+	</div>
+	
+	 <div id="copyright" >Your copyright slogan here || <?php echo SVRNAME; ?> || Owner: <?php echo OWNER; ?> || <?php echo YEAR; ?></div>
+	 
+	 
+ </body>
+ 
---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	//Make sure to edit all of these settings below
+	define('SVRNAME', 'Your Server Name');
+	define('OWNER', 'Your name'); //Or the community owners name
+	define('SVRIP', '192.168.1.1'); //Your server's IP
+	define('YEAR', '2014'); //This is just for the copyright, probably wont need to change it
+
+?>
